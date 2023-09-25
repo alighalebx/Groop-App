f4205417f500f6f6499471596cf3bff4e02f023e
Ali Ghaleb <alighaleb2001@gmail.com>
2023-09-25 17:01:24 +0300

    Controller

--- src/main/java/com/rungroop/web/repository/ClubRepository.java
+++ src/main/java/com/rungroop/web/repository/ClubRepository.java
@@ -3,5 +3,9 @@ package com.rungroop.web.repository;
 import com.rungroop.web.models.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClubRepository extends JpaRepository<Club , Long> {
+
+    Optional<Club>findByTitle(String Url);
 }
